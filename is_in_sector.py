ca1c6932040125b789163945a5abf5d5d6fc75eb
fakivanova <fakivanova@gmail.com>
2019-03-21 11:44:16 +0300

    update

--- is_in_sector.py
+++ is_in_sector.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 import sys
 import argparse
 
@@ -10,11 +10,12 @@ def is_in_sector(v_target, alpha, gamma, r):
         return False 
 	alpha_max = alpha + gamma / 2
 	alpha_min = alpha - gamma / 2
-    if (not is_clockwise((math.cos(alpha_max), math.sin(alpha_max)), v_target) 
-        and is_clockwise((math.cos(alpha_min), math.sin(alpha_min)), v_target)):
+    if 		(not is_clockwise((math.cos(alpha_max), math.sin(alpha_max)), v_target) 
+        	and is_clockwise((math.cos(alpha_min), math.sin(alpha_min)), v_target)):
         return False 
     return True 
 
 if __name__ == '__main__':
 	# arguments x_target y_target x_station y_station alpha_station gamma_station r_station
 	parser = argparse.ArgumentParser()
+	parser.add_argument('x_target', type=int)
