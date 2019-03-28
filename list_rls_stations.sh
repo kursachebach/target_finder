b56cf4c9088bdc9dd8de87c426c7137f8a743b93
fakivanova <fakivanova@gmail.com>
2019-03-28 11:09:36 +0300

    update

--- list_rls_stations.sh
+++ list_rls_stations.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Выводит параметры всех рлс в формате:
+# rls_id x y alpha sweep radius
 
 rls_stations_file="rls_stations.txt"
 rls_characteristics_file="rls_characteristics.txt"
@@ -19,3 +21,27 @@ while read city_name city_x city_y; do
     fi 
     city_coordinates[${city_name}]="${city_x} ${city_y}"
 done < "${city_coordinates_file}"
+
+while read station_id place alpha station_name; do
+    if [[ ${station_id} == "#"* ]]; then
+        continue
+    fi
+    if [[ ${place} == "x="* ]]; then
+        x=${place%,y=*}
+        x=${x:2}
+        y=${place#*y=}
+    else
+        coordinates=(${city_coordinates[${place}]})
+        x=${coordinates[0]}
+        y=${coordinates[1]}
+    fi
+    if [[ ${station_name} == "r="* ]]; then
+        radius=${station_name#r=}
+        sweep=360
+    else
+        characteristics=(${rls_characteristics[${station_name}]})
+        sweep=${characteristics[0]}
+        radius=${characteristics[1]}
+    fi
+    echo ${station_id} ${x} ${y} ${alpha} ${sweep} ${radius}
+done < "${rls_stations_file}"
